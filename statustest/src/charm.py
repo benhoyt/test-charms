61f12a064f52d1b28ebe3339f92d964ebfe3c238
Ben Hoyt <ben.hoyt@canonical.com>
2023-05-22 17:02:09 +1200

    Simple test charm using OpenStack's version of StatusPool

--- statustest/src/charm.py
+++ statustest/src/charm.py
@@ -3,13 +3,72 @@
 import logging
 
 import ops
+import statuspool
 
 logger = logging.getLogger(__name__)
 
 
 class StatustestCharm(ops.CharmBase):
+    """Status test charm."""
+
     def __init__(self, *args):
         super().__init__(*args)
+
+        status_pool = statuspool.StatusPool(self)
+        self.database = Database(self, status_pool)
+        self.webapp = Webapp(self, status_pool)
+
+
+class Database(ops.Object):
+    """Database component."""
+
+    def __init__(self, charm, status_pool):
+        super().__init__(charm, "database")
+
+        self.charm = charm
+        self.status = statuspool.Status("database")
+        status_pool.add(self.status)
+        charm.framework.observe(charm.on.config_changed, self._on_config_changed)
+
+        self._update_config()
+
+    def _on_config_changed(self, event):
+        self._update_config()
+
+    def _update_config(self):
+        if "database_mode" not in self.charm.model.config:
+            self.status.set(ops.BlockedStatus('"database_mode" required'))
+            return
+
+        mode = self.charm.model.config["database_mode"]
+        logger.info("Using database mode %r", mode)
+        self.status.set(ops.ActiveStatus(f"db mode {mode!r}"))
+
+
+class Webapp(ops.Object):
+    """Web app component."""
+
+    def __init__(self, charm, status_pool):
+        super().__init__(charm, "webapp")
+
+        self.charm = charm
+        self.status = statuspool.Status("webapp")
+        status_pool.add(self.status)
+        charm.framework.observe(charm.on.config_changed, self._on_config_changed)
+
+        self._update_config()
+
+    def _on_config_changed(self, event):
+        self._update_config()
+
+    def _update_config(self):
+        if "webapp_port" not in self.charm.model.config:
+            self.status.set(ops.BlockedStatus('"webapp_port" required'))
+            return
+
+        port = self.charm.model.config["webapp_port"]
+        logger.info("Using web app port %r", port)
+        self.status.set(ops.ActiveStatus(f"web app port {port!r}"))
 
 
 if __name__ == "__main__":  # pragma: nocover
--- statustest/tests/unit/test_charm.py
+++ statustest/tests/unit/test_charm.py
@@ -2,7 +2,6 @@ import unittest
 
 import ops
 import ops.testing
-
 from charm import StatustestCharm
 
 
@@ -12,5 +11,25 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_thing(self):
-        pass
+    def test_initial(self):
+        status = self.harness.model.unit.status
+        self.assertEqual(status.name, "blocked")
+        self.assertEqual(status.message, '(database) "database_mode" required')
+
+    def test_database_mode_set(self):
+        self.harness.update_config({"database_mode": "single"})
+        status = self.harness.model.unit.status
+        self.assertEqual(status.name, "blocked")
+        self.assertEqual(status.message, '(webapp) "webapp_port" required')
+
+    def test_webapp_port_set(self):
+        self.harness.update_config({"webapp_port": 8080})
+        status = self.harness.model.unit.status
+        self.assertEqual(status.name, "blocked")
+        self.assertEqual(status.message, '(database) "database_mode" required')
+
+    def test_all_config_set(self):
+        self.harness.update_config({"database_mode": "single", "webapp_port": 8080})
+        status = self.harness.model.unit.status
+        self.assertEqual(status.name, "active")
+        self.assertEqual(status.message, "(database) db mode 'single'")
