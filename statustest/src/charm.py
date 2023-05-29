8adb270da8c47e9924466f4c015d08a5cdf75453
Ben Hoyt <ben.hoyt@canonical.com>
2023-05-29 17:19:24 +1200

    Little bit of cleanup for spec

--- statustest/src/charm.py
+++ statustest/src/charm.py
@@ -1,6 +1,7 @@
 """Multi-status test charm for OP033 spec.."""
 
 import logging
+import typing
 
 import multistatus
 import ops
@@ -13,10 +14,8 @@ class StatustestCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-
         self.database = Database(self)
         self.webapp = Webapp(self)
-
         self.prioritizer = multistatus.Prioritizer()
         self.prioritizer.add("database", self.database.get_status)
         self.prioritizer.add("webapp", self.webapp.get_status)
@@ -31,21 +30,28 @@ class Database(ops.Object):
 
     def __init__(self, charm):
         super().__init__(charm, "database")
-
         self.charm = charm
         charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
-    def _on_config_changed(self, event):
+    def get_status(self) -> ops.StatusBase:
+        """Return this component's status."""
+        status = self._validate_config()
+        return status if status is not None else ops.ActiveStatus()
+
+    def _validate_config(self) -> typing.Optional[ops.StatusBase]:
+        """Validate charm config for the database component.
+
+        Return a status if the config is incorrect, None if it's valid.
+        """
         if "database_mode" not in self.charm.model.config:
+            return ops.BlockedStatus('"database_mode" required')
+        return None
+
+    def _on_config_changed(self, event):
+        if self._validate_config() is not None:
             return
         mode = self.charm.model.config["database_mode"]
         logger.info("Using database mode %r", mode)
-
-    def get_status(self) -> ops.StatusBase:
-        """Return this component's status."""
-        if "database_mode" not in self.charm.model.config:
-            return ops.BlockedStatus('"database_mode" required')
-        return ops.ActiveStatus()
 
 
 class Webapp(ops.Object):
@@ -53,21 +59,28 @@ class Webapp(ops.Object):
 
     def __init__(self, charm):
         super().__init__(charm, "webapp")
-
         self.charm = charm
         charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
-    def _on_config_changed(self, event):
+    def get_status(self) -> ops.StatusBase:
+        """Return this component's status."""
+        status = self._validate_config()
+        return status if status is not None else ops.ActiveStatus()
+
+    def _validate_config(self) -> typing.Optional[ops.StatusBase]:
+        """Validate charm config for the web app component.
+
+        Return a status if the config is incorrect, None if it's valid.
+        """
         if "webapp_port" not in self.charm.model.config:
+            return ops.BlockedStatus('"webapp_port" required')
+        return None
+
+    def _on_config_changed(self, event):
+        if self._validate_config() is not None:
             return
         port = self.charm.model.config["webapp_port"]
         logger.info("Using web app port %r", port)
-
-    def get_status(self) -> ops.StatusBase:
-        """Return this component's status."""
-        if "webapp_port" not in self.charm.model.config:
-            return ops.BlockedStatus('"webapp_port" required')
-        return ops.ActiveStatus()
 
 
 if __name__ == "__main__":  # pragma: nocover
