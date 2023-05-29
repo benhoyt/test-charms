8d17482fc5ef19e01c4b1e5c7f743c6347902b69
Ben Hoyt <ben.hoyt@canonical.com>
2023-05-29 16:06:20 +1200

    Added multistatus.Group version

--- statustest/src/charm.py
+++ statustest/src/charm.py
@@ -1,9 +1,9 @@
-"""Multi-status test charm for OP033 spec.."""
+"""Multi-status test charm for OP033 spec."""
 
 import logging
 
+import multistatus
 import ops
-import statuspool
 
 logger = logging.getLogger(__name__)
 
@@ -14,20 +14,19 @@ class StatustestCharm(ops.CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        status_pool = statuspool.StatusPool(self)
-        self.database = Database(self, status_pool)
-        self.webapp = Webapp(self, status_pool)
+        status_group = multistatus.Group(self)
+        self.database = Database(self, status_group)
+        self.webapp = Webapp(self, status_group)
 
 
 class Database(ops.Object):
     """Database component."""
 
-    def __init__(self, charm, status_pool):
+    def __init__(self, charm, status_group):
         super().__init__(charm, "database")
 
         self.charm = charm
-        self.status = statuspool.Status("database")
-        status_pool.add(self.status)
+        self.component = multistatus.Component(status_group, "database")
         charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
         self._update_config()
@@ -37,23 +36,22 @@ class Database(ops.Object):
 
     def _update_config(self):
         if "database_mode" not in self.charm.model.config:
-            self.status.set(ops.BlockedStatus('"database_mode" required'))
+            self.component.status = ops.BlockedStatus('"database_mode" required')
             return
 
         mode = self.charm.model.config["database_mode"]
         logger.info("Using database mode %r", mode)
-        self.status.set(ops.ActiveStatus(f"db mode {mode!r}"))
+        self.component.status = ops.ActiveStatus(f"db mode {mode!r}")
 
 
 class Webapp(ops.Object):
     """Web app component."""
 
-    def __init__(self, charm, status_pool):
+    def __init__(self, charm, status_group):
         super().__init__(charm, "webapp")
 
         self.charm = charm
-        self.status = statuspool.Status("webapp")
-        status_pool.add(self.status)
+        self.component = multistatus.Component(status_group, "webapp")
         charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
         self._update_config()
@@ -63,12 +61,12 @@ class Webapp(ops.Object):
 
     def _update_config(self):
         if "webapp_port" not in self.charm.model.config:
-            self.status.set(ops.BlockedStatus('"webapp_port" required'))
+            self.component.status = ops.BlockedStatus('"webapp_port" required')
             return
 
         port = self.charm.model.config["webapp_port"]
         logger.info("Using web app port %r", port)
-        self.status.set(ops.ActiveStatus(f"web app port {port!r}"))
+        self.component.status = ops.ActiveStatus(f"web app port {port!r}")
 
 
 if __name__ == "__main__":  # pragma: nocover
---
+++ statustest/src/multistatus.py
@@ -0,0 +1,108 @@
+"""Status prioritizer."""
+
+import typing
+
+import ops
+
+
+class Prioritizer:
+    """Status prioritizer: track the highest-priority status among several components."""
+
+    _PRIORITIES = {
+        "error": 0,
+        "blocked": 1,
+        "waiting": 2,
+        "maintenance": 3,
+        "active": 4,
+        "unknown": 5,
+    }
+
+    def __init__(self):
+        self._components = {}
+
+    def add(self, component: str, get_status: typing.Callable[[], ops.StatusBase]):
+        """Add a named status component."""
+        if component in self._components:
+            raise ValueError(f"duplicate component {component!r}")
+        self._components[component] = get_status
+
+    def highest(self) -> ops.StatusBase:
+        """Return highest-priority status with message prefixed with component name."""
+        statuses = self.all()
+        if not statuses:
+            return ops.ActiveStatus()
+        component, status = statuses[0]
+        if isinstance(status, ops.ActiveStatus) and not status.message:
+            return ops.ActiveStatus()
+        return ops.StatusBase.from_name(status.name, f"[{component}] {status.message}")
+
+    def all(self) -> list[tuple[str, ops.StatusBase]]:
+        """Return list of (component_name, status) tuples for all components.
+
+        The list is ordered highest-priority first. If there are two statuses
+        with the same level, components added first come first.
+        """
+        # TODO: exception handling (log full details and yield ErrorStatus?)
+        statuses = [
+            (component, get_status()) for component, get_status in self._components.items()
+        ]
+        statuses.sort(key=lambda s: self._PRIORITIES[s[1].name])
+        return statuses
+
+
+class Group:
+    """Group of status-having components.
+
+    Each component's status is saved to stored state on update, and loaded
+    when the charm is initialized.
+    """
+
+    def __init__(self, charm: ops.CharmBase, app=False):
+        self._charm = charm
+        self._app = app
+        self._prioritizer = Prioritizer()
+        self._components = {}
+        self._loaded = {}
+        self._load()
+
+    def _add(self, component: "Component") -> None:
+        self._components[component.name] = component
+        self._prioritizer.add(component.name, lambda: component.status)
+        loaded = self._loaded.get(component.name)
+        if loaded is not None:
+            component._status = loaded
+
+    def _update(self) -> None:
+        highest = self._prioritizer.highest()
+        if self._app:
+            self._charm.app.status = highest
+        else:
+            self._charm.unit.status = highest
+        self._save()
+
+    def _load(self):
+        pass  # TODO: populate self._loaded from storage
+
+    def _save(self):
+        pass  # TODO: iterate through components and save to storage
+
+
+class Component:
+    """Single status-having component in a status group."""
+
+    def __init__(self, group: Group, name: str, initial=ops.UnknownStatus()):  # TODO: or active?
+        self.group = group
+        self.name = name
+        self._status = initial
+        self.group._add(self)
+
+    @property
+    def status(self) -> ops.StatusBase:
+        """The component's status."""
+        return self._status
+
+    @status.setter
+    def status(self, value: ops.StatusBase):
+        """Set the component's status (and save the status group)."""
+        self._status = value
+        self.group._update()
